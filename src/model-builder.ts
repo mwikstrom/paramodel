41cae9e4d83445f472a5843abc24edb5e3e4aab9
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-07-31 01:44:31 +0200

    setup domain (partial)

--- src/model-builder.ts
+++ src/model-builder.ts
@@ -1,4 +1,4 @@
-import { Type } from "paratype";
+import { Type, voidType } from "paratype";
 import { ActionHandler } from "./action-handler";
 import { ChangeModel, ReadModel, WriteModel, DomainModel } from "./model";
 import { Projection } from "./projection";
@@ -10,19 +10,40 @@ export interface ModelBuilder<
     Scope = unknown,
 > {
     addEvent<EventKey extends string, EventArg>(
+        this: void,
         key: EventKey, 
         type: Type<EventArg>,
     ): ModelBuilder<Events & ChangeModel<EventKey, EventArg>, Views, Actions, Scope>;
 
     addView<ViewKey extends string, Handler extends Projection>(
+        this: void,
         key: ViewKey,
         handler: Handler,
     ): ModelBuilder<Events, Views & ReadModel<ViewKey, Handler>, Actions, Scope>;
 
     addAction<ActionKey extends string, Handler extends ActionHandler>(
+        this: void,
         key: ActionKey,
         handler: Handler,
     ): ModelBuilder<Events, Views, Actions & WriteModel<ActionKey, ActionHandler>, Scope>;
 
-    createModel(): DomainModel<Events, Views, Actions, Scope>;
+    createModel(this: void): DomainModel<Events, Views, Actions, Scope>;
+}
+
+export function setupDomain(): ModelBuilder<ChangeModel, ReadModel, WriteModel, void>;
+export function setupDomain<Scope>(scope: Type<Scope>): ModelBuilder<ChangeModel, ReadModel, WriteModel, Scope>;
+export function setupDomain<Scope>(
+    scope: Type<Scope | void> = voidType,
+): ModelBuilder<ChangeModel, ReadModel, WriteModel, Scope | void> {
+    const createModel = () => Object.freeze({
+    });
+
+    const builder = Object.freeze({
+        addEvent,
+        addView,
+        addAction,
+        createModel,
+    });
+
+    return builder;
 }
