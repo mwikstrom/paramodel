3a876c05d1c6c92a653b00ae6c1c3fef08fe2365
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-07-30 23:37:15 +0200

    todo

--- src/provider.ts
+++ src/provider.ts
@@ -1,7 +1,7 @@
 import { DomainModel } from "./model";
 import { DomainStore } from "./store";
 
-// TODO: DomainContext: user auth stuff (expose in action and view handlers)
+// TODO: Domain call context: user auth stuff (expose in action and view handlers)
 export interface DomainStoreProvider {
     get<Model extends DomainModel>(
         id: string, 
