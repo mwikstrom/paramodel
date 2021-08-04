f9ab3de380d6db7b9737154bbd8b14112a3eff6f
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-08-04 10:21:53 +0200

    impl memory partition

--- src/internal/memory-partition.ts
+++ src/internal/memory-partition.ts
@@ -5,21 +5,65 @@ import { _TimeSource } from "./time-source";
 export class _MemoryPartition {
     readonly #time: _TimeSource;
     readonly #records = new Map<string, StoredRecord>();
+    #tokenCounter = 0;
 
     constructor(time: _TimeSource) {
         this.#time = time;
     }
 
+    #hasExpired = (record: StoredRecord): boolean => {
+        const { timestamp, ttl } = record;
+        const now = this.#time.now();
+        return ttl >= 0 && now >= timestamp.valueOf() + ttl * 1000;
+    }
+
     all = (): readonly OutputRecord[] => {
-        throw new Error("TODO: Method not implemented.");
+        const result: OutputRecord[] = [];
+        const purge: string[] = [];
+
+        for (const [key, stored] of this.#records) {
+            if (this.#hasExpired(stored)) {
+                purge.push(key);
+            } else {
+                result.push({ ...stored, key });
+            }
+        }
+
+        for (const key of purge) {
+            this.#records.delete(key);
+        }
+
+        return result;
     }
 
     read = (key: string): OutputRecord | undefined => {
-        throw new Error("TODO: Method not implemented.");
+        const stored = this.#records.get(key);
+
+        if (!stored) {
+            return void(0);
+        }
+
+        if (this.#hasExpired(stored)) {
+            this.#records.delete(key);
+            return void(0);
+        }
+        
+        return { ...stored, key };
     }
 
     write = (input: InputRecord): boolean => {
-        throw new Error("TODO: Method not implemented.");
+        const { key, value, replace, ttl } = input;
+        const output = this.read(key);
+
+        if ((!output && replace !== null) || (output && replace !== output.token)) {
+            return false;
+        }
+
+        const token = (++this.#tokenCounter).toString(10);
+        const timestamp = new Date(this.#time.now());
+        const stored: StoredRecord = Object.freeze({ value, token, ttl, timestamp });
+        this.#records.set(key, stored);
+        return true;
     }
 }
 
