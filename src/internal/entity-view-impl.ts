ab454b6b6ba1b4ff5328091699c067f69f77dee0
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-08-05 11:25:01 +0200

    queryable component

--- src/internal/entity-view-impl.ts
+++ src/internal/entity-view-impl.ts
@@ -1,7 +1,7 @@
 import { positiveIntegerType, recordType, Type } from "paratype";
 import { DomainDriver, FilterSpec, OutputRecord } from "../driver";
 import { EntityView, PossibleKeysOf } from "../entity-view";
-import { Queryable } from "../queryable";
+import { FilterOperand, FilterOperator, Page, PageOptions, Queryable, SortDirection } from "../queryable";
 import { _QueryImpl } from "./query-impl";
 import { _DriverQuerySource } from "./query-source";
 
@@ -9,10 +9,12 @@ import { _DriverQuerySource } from "./query-source";
 export class _EntityViewImpl<
     T extends {[P in K]: string | number},
     K extends PossibleKeysOf<T>
-> extends _QueryImpl<T> implements EntityView<T, K> {
+> implements EntityView<T, K> {
+    #query: Queryable<T & {[P in K]: string | number}>;
     #keyProp: K;
+    #authed: undefined | boolean;
     public readonly kind = "entities";
-    public readonly version: number;
+    public readonly version: number;    
 
     constructor(
         driver: DomainDriver,
@@ -44,20 +46,41 @@ export class _EntityViewImpl<
             }
         ]);
 
-        super(source, ["value", "entity"], where);
+        this.#query = new _QueryImpl(source, ["value", "entity"], where);
         this.#keyProp = keyProp;
         this.version = version;
     }
 
-    auth = (): Promise<boolean> => {
-        throw new Error("TODO: Method not implemented.");
+    auth = async (): Promise<boolean> => {
+        if (this.#authed === void(0)) {
+            throw new Error("TODO: Method not implemented.");
+        }
+        
+        return this.#authed;
     }
 
-    get = (key: T[K]): Promise<T | undefined> => (
-        (this as Queryable<T & {[P in K]: string | number}>)
-            .where(this.#keyProp, "==", key)
-            .first()
-    );
+    get = (key: T[K]): Promise<T | undefined> => this.#query.where(this.#keyProp, "==", key).first();
+
+    all = (): AsyncIterable<T> => this.#query.all();
+    
+    any = async (): Promise<boolean> => this.#query.any();
+    
+    by = <P extends string & keyof T>(
+        property: P,
+        direction: SortDirection = "ascending",
+    ): Queryable<T> => this.#query.by(property, direction);
+
+    count = (): Promise<number> => this.#query.count();
+    
+    first = async (): Promise<T | undefined> => this.#query.first();
+    
+    page = (options: PageOptions = {}): Promise<Page<T>> => this.#query.page(options);
+
+    where = <P extends string & keyof T, O extends FilterOperator<T[P]>>(
+        property: P,
+        operator: O,
+        operand: FilterOperand<T[P], O>
+    ): Queryable<T> => this.#query.where(property, operator, operand);
 }
 
 type Envelope<T> = {
