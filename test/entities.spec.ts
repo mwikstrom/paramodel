c0bdeaf39b8f8a3f1f13cf589b25aee8c0484b2f
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-08-07 11:42:40 +0200

    split test

---
+++ test/entities.spec.ts
@@ -0,0 +1,26 @@
+import { createAccountStore } from "./create-account-store";
+
+describe("DomainStore.view-entities", () => {
+    it("can read initial entities", async () => {
+        const store = await createAccountStore();
+        const view = await store.view("accounts");
+        expect(await view?.count()).toBe(0);
+    });
+
+    it("can read entity after commit", async () => {
+        const store = await createAccountStore();
+        const result = await store.do("register_account", { owner_id: "jane_doe" });
+        const view = await store.view("accounts", { sync: result.committed });
+        expect(await view?.count()).toBe(1);
+    });
+
+    it("can read entity before and after sync", async () => {
+        const store = await createAccountStore();
+        await store.do("register_account", { owner_id: "jane_doe" });
+        const before = await store.view("accounts");
+        await store.sync();
+        const after = await store.view("accounts");
+        expect(await before?.count()).toBe(0);
+        expect(await after?.count()).toBe(1);
+    });
+});
--- test/state.spec.ts
+++ test/state.spec.ts
@@ -1,35 +1,6 @@
-import { RegisterAccount } from "./account-domain/register-account";
-import { AccountActions } from "./account-domain/write-model";
 import { createAccountStore } from "./create-account-store";
 
-describe("DomainStore", () => {
-    it("rejects unknown action", async () => {
-        const store = await createAccountStore();
-        const result = await store.do("incorrect" as keyof AccountActions, {} as RegisterAccount);
-        expect(result.status).toBe("rejected");
-        expect(result.message).toBe("Unknown action: incorrect");
-    });
-
-    it("rejects invalid action input", async () => {
-        const store = await createAccountStore();
-        const result = await store.do("register_account", {} as RegisterAccount);
-        expect(result.status).toBe("rejected");
-        expect(result.message).toBe("Invalid action input: Missing required property: owner_id");
-    });
-
-    it("valid action is accepted", async () => {
-        const store = await createAccountStore();
-        const result = await store.do("register_account", { owner_id: "jane_doe" });
-        expect(result.status).toBe("success");
-        expect(result.output).toBe(1);
-    });
-
-    it("action can be forbidden", async () => {
-        const store = await createAccountStore();
-        const result = await store.do("register_account", { owner_id: "other" });
-        expect(result.status).toBe("forbidden");
-    });
-
+describe("DomainStore.view-state", () => {
     it("can read initial state", async () => {
         const store = await createAccountStore();
         const view = await store.view("next_account_id");
