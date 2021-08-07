3fe1ddeeaa21f6f25b944c1c2f84335ab3d359d0
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-08-07 20:25:50 +0200

    coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ const config = {
     preset: "ts-jest/presets/default-esm",
     testEnvironment: "node",
     testRegex: "test/.*\\.spec\\.ts$",
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    collectCoverageFrom: ["src/**/*.ts"],
     globals: {
         "ts-jest": {
             useESM: true
