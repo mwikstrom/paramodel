931f0291d431a2c068e9abc39ea67153656b95a1
Mårten Wikström <marten.wikstrom@sigmatechnology.se>
2021-08-07 09:38:41 +0200

    fake without using any

--- test/store.spec.ts
+++ test/store.spec.ts
@@ -1,9 +1,11 @@
+import { RegisterAccount } from "./account-domain/register-account";
+import { AccountActions } from "./account-domain/write-model";
 import { createAccountStore } from "./create-account-store";
 
 describe("DomainStore", () => {
     it("rejects unknown action", async () => {
         const store = await createAccountStore();
-        const result = await store.do("incorrect" as any, null as any);
+        const result = await store.do("incorrect" as keyof AccountActions, {} as RegisterAccount);
         expect(result.status).toBe("rejected");
         expect(result.message).toBe("???");
     });
